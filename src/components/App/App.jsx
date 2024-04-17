a416f043827396692e4ba34afcaf3e8a6c736af4
HMAsp <hanand55935@stud.noroff.no>
2024-04-17 09:02:31 +0200

    Refactor project structure and setting up tailwind

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">HELLO WORLD</h1>
+      <button className="">test</button>
+    </>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      orange: "#AD4000",
+      yellow: "#F2AF5C",
+      brown: "#8C4F2B",
+      offWhite: "#FDF2DC",
+      teal: "#8697A6",
+    },
     extend: {},
   },
   plugins: [],
