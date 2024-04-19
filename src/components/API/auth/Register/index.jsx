438d24590df8357c53535929f7d38b8095227d85
HMAsp <hanand55935@stud.noroff.no>
2024-04-19 11:53:27 +0200

    Adds Register fetch Component

---
+++ src/components/API/auth/Register/index.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const Register = (method, body = null) => {
+  const baseUrl = "https://v2.api.noroff.dev/";
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(baseUrl, {
+          method: method,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        });
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        setError(error);
+      }
+      setLoading(false);
+    }
+    fetchData();
+  }, [method, body]);
+  return { loading, error, data };
+};
+
+export default Register;
